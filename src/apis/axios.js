9cf909cd4eb1f989b9e8021902388e497d313f26
Lek Peng <lekpeng96@hotmail.com>
2022-11-03 21:08:12 +0800

    add credentials for base url

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -3,6 +3,7 @@ const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 export default axios.create({
   baseURL: BASE_URL,
+  withCredentials: true,
 });
 
 export const axiosPrivate = axios.create({
