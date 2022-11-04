e4b2ee335fce0f0d06c8a2165e88e90169d216f4
Lek Peng <lekpeng96@hotmail.com>
2022-11-05 00:03:36 +0800

    feat: delete product

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import ProductCard from "./product_components/ProductCard";
 import productApis from "../../apis/product";
 import toast from "react-hot-toast";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Box } from "@mui/system";
 import { Button } from "@mui/material";
 import useAuth from "../../hooks/useAuth";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 function Product() {
   const [product, setProduct] = useState({});
+  const navigate = useNavigate();
   const params = useParams();
   const { auth } = useAuth();
   useEffect(() => {
@@ -22,12 +24,24 @@ function Product() {
     showProduct();
   }, [params.productId]);
 
+  const axiosPrivate = useAxiosPrivate();
+
+  const handleDelete = async () => {
+    try {
+      const response = await axiosPrivate.delete(`/products/${params.productId}`);
+      toast.success(`Deleted ${product.name} successfully`);
+      navigate(`/merchants/${product.MerchantId}`);
+    } catch (err) {
+      toast.error(err.response.data.error);
+    }
+  };
+
   return (
     <Box sx={{ width: "345px", margin: "auto" }}>
       {auth?.user?.userType === "Merchant" && auth?.user?.email === product?.Merchant?.email ? (
         <Box sx={{ mb: 3 }}>
-          <Button variant="outlined">Edit</Button>{" "}
-          <Button color="error" variant="outlined">
+          <Button variant="outlined">Edit</Button>
+          <Button sx={{ ml: 1 }} onClick={handleDelete} color="error" variant="outlined">
             Delete
           </Button>
         </Box>
